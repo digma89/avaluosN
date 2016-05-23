700672db951cd0bc0049ab6c26c59845ee431a94
digma89 <digma89@hotmail.com>
2016-05-23 12:10:03 -0400

    V.5.0.2

--- public/example/config/example.client.routes.js
+++ public/example/config/example.client.routes.js
@@ -23,3 +23,4 @@ angular.module('example').config(['$routeProvider',
     }
 ]);
 
+
--- public/example/controllers/home.client.controller.js
+++ public/example/controllers/home.client.controller.js
@@ -5,3 +5,4 @@ angular.module('example').controller('ExampleController', ['$scope', '$location'
         };
     },
 ]);
+
