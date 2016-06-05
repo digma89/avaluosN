f8d5e8b8ac089a7435bf368883f6f5b85319f1f8
digma89 <digma89@hotmail.com>
2016-06-04 21:30:44 -0400

    V.11.0.0 Slider

--- public/example/controllers/home.client.controller.js
+++ public/example/controllers/home.client.controller.js
@@ -6,3 +6,43 @@ angular.module('example').controller('ExampleController', ['$scope', '$location'
     },
 ]);
 
+angular.module('example').controller('CarouselDemoCtrl', function($scope) {
+    $scope.myInterval = 4000;
+    $scope.noWrapSlides = true;
+    $scope.active = 0;
+    var slides = $scope.slides = [];
+    var currIndex = 0;
+
+    $scope.addSlide = function() {
+        var newWidth = 600 + slides.length + 1;
+        slides.push({
+            image: 'images/slider/slide1.png',
+            id: currIndex++
+        },
+            {
+                image: 'images/slider/slide2.png',
+                id: currIndex++
+            },
+            {
+                image: 'images/slider/slide3.png',
+                id: currIndex++
+            },
+            {
+                image: 'images/slider/slide4.png',
+                id: currIndex++
+            },
+            {
+                image: 'images/slider/slide5.png',
+                id: currIndex++
+            });
+    };
+
+    for (var i = 0; i < 1; i++) {
+        if (currIndex >= 5)
+            currIndex = 0;
+        $scope.addSlide();
+    }
+
+
+});
+
--- public/lib/angular-bootstrap/ui-bootstrap-tpls.js
+++ public/lib/angular-bootstrap/ui-bootstrap-tpls.js
@@ -7002,7 +7002,7 @@ angular.module("uib/template/carousel/carousel.html", []).run(["$templateCache",
     "    <span aria-hidden=\"true\" class=\"glyphicon glyphicon-chevron-right\"></span>\n" +
     "    <span class=\"sr-only\">next</span>\n" +
     "  </a>\n" +
-    "  <ol class=\"carousel-indicators\" ng-show=\"slides.length > 1\">\n" +
+    "  <ol class=\"carousel-indicators\" ng-show=\"!hideIndicators && slides.length> 1\">\n" +
     "    <li ng-repeat=\"slide in slides | orderBy:indexOfSlide track by $index\" ng-class=\"{ active: isActive(slide) }\" ng-click=\"select(slide)\">\n" +
     "      <span class=\"sr-only\">slide {{ $index + 1 }} of {{ slides.length }}<span ng-if=\"isActive(slide)\">, currently active</span></span>\n" +
     "    </li>\n" +
